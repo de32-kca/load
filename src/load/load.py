2d13a596a1a4baf6633e8af04efd5fb7f40848f0
Mingk42 <xoals123456t@gmail.com>
2024-08-06 11:59:13 +0900

    load init

---
+++ src/load/load.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from tabulate import tabulate
+
+
+def load_data(path="~/code/de32-kca/transform_kca"):
+    df=pd.read_parquet("~/code/de32-kca/transform_kca")
+
+    for c in ["salesAmt","audiCnt","scrnCnt","showCnt"]:
+        df[c]=df[c].astype(int)
+
+    cnt=df.groupby("movieCd").count()["load_dt"]
+
+    movieList=df[["movieCd","movieNm"]].drop_duplicates()
+
+    cntByMovie=movieList.merge(cnt, on="movieCd").sort_values("load_dt",ascending=False)
+    cntByMovie.columns=["movieCd","movieNm","movieCnt"]
+
+    print(tabulate(cntByMovie, tablefmt="outline"))
+    #return cntByMovie
